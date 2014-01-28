b0a602c03e0967fb1cea0ea6932517cce7a54347
Steven 'Diagram' Sinatra <diagramatics@gmail.com>
2014-01-28 23:34:57 +1100

    Initial commit.

---
+++ input.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+	<title>Foundation Programming Task</title>
+</head>
+
+<body>
+	<form action="">
+		<div class="form-section-container"><span class="form-text">First Name</span><input type="text" class="form-input"></div>
+		<div class="form-section-container"><span class="form-text">Last Name</span><input type="text" class="form-input"></div>
+		<div class="form-section-container"><span class="form-text">Street Address</span><input type="text" class="form-input"></div>
+		<div class="form-section-container"><span class="form-text">Suburb</span><input type="text" class="form-input"></div>
+		<div class="form-section-container"><span class="form-text">State</span><input type="text" class="form-input"></div>
+		<div class="form-section-container"><span class="form-text">Postal Code</span><input type="text" class="form-input"></div>
+		<div class="form-section-container"><span class="form-text">Telephone Number</span><input type="text" class="form-input"></div>
+		<div class="form-section-container"><span class="form-text">Email</span><input type="text" class="form-input"></div>
+		<input type="submit" text="Register">
+	</form>
+</body>
+
+<html>
