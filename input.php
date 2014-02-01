3c132f12ffbe0aa5715cd36afc333f695c3a1601
Steven 'Diagram' Sinatra <diagramatics@gmail.com>
2014-02-01 21:39:05 +1100

    Finishing up Javascript validation.

--- input.php
+++ input.php
@@ -6,22 +6,22 @@
 </head>
 
 <body>
-	<form action="">
+	<form action="" method="POST" onsubmit="return formCheck(this)">
 		<div class="form-section-container">
-			<span class="form-text">First Name</span><input required type="text" name="fn" class="form-input" data-limit="20" data-allow="^[a-z']+$" oninput="checkFormInput(event)">
-			<span id="fn-error"></span>
+			<span class="form-text">First Name</span><input required type="text" name="fn" class="form-input" data-limit="20" data-allow="^[a-z']+$" onblur="checkFormInput(this)" onchange="checkFormInput(this)">
+			<span class="form-error" id="fn-error"></span>
 		</div>
 		<div class="form-section-container">
-			<span class="form-text">Last Name</span><input required type="text" name="ln" class="form-input" data-limit="30" data-allow="^[a-z']+$" oninput="checkFormInput(event)">
-			<span id="ln-error"></span>
+			<span class="form-text">Last Name</span><input required type="text" name="ln" class="form-input" data-limit="30" data-allow="^[a-z']+$" onblur="checkFormInput(this)" onchange="checkFormInput(this)">
+			<span class="form-error" id="ln-error"></span>
 		</div> 
 		<div class="form-section-container">
-			<span class="form-text">Street Address</span><input required type="text" name="street" class="form-input" data-limit="50" data-allow="^[\sa-z0-9]+$" oninput="checkFormInput(event)">
-			<span id="street-error"></span>
+			<span class="form-text">Street Address</span><input required type="text" name="street" class="form-input" data-limit="50" data-allow="^[\sa-z0-9]+$" onblur="checkFormInput(this)" onchange="checkFormInput(this)">
+			<span class="form-error" id="street-error"></span>
 		</div>
 		<div class="form-section-container">
-			<span class="form-text">Suburb</span><input required type="text" name="suburb" class="form-input" data-limit="15" data-allow="^[a-z']+$" oninput="checkFormInput(event)">
-			<span id="suburb-error"></span>
+			<span class="form-text">Suburb</span><input required type="text" name="suburb" class="form-input" data-limit="15" data-allow="^[a-z']+$" onblur="checkFormInput(this)" onchange="checkFormInput(this)">
+			<span class="form-error" id="suburb-error"></span>
 		</div>
 		<div class="form-section-container">
 			<span class="form-text">State</span>
@@ -38,16 +38,16 @@
 			</select>
 		</div>
 		<div class="form-section-container">
-			<span class="form-text">Postal Code</span><input required type="text" name="postal" class="form-input" data-limit="4" data-allow="^[0-9]+$" oninput="checkFormInput(event)">
-			<span id="postal-error"></span>
+			<span class="form-text">Postal Code</span><input required type="text" name="postal" class="form-input" data-limit="4" data-allow="^[0-9]+$" onblur="checkFormInput(this)" onchange="checkFormInput(this)">
+			<span class="form-error" id="postal-error"></span>
 		</div>
 		<div class="form-section-container">
-			<span class="form-text">Telephone Number</span><input required type="tel" name="phone" class="form-input" data-limit="10" data-allow="^[0-9+]+$" oninput="checkFormInput(event)">
-			<span id="phone-error"></span>
+			<span class="form-text">Telephone Number</span><input required type="tel" name="phone" class="form-input" onblur="checkPhoneInput(this)" onchange="checkPhoneInput(this)">
+			<span class="form-error" id="phone-error"></span>
 		</div>
 		<div class="form-section-container">
-			<span class="form-text">Email</span><input required type="email" name="email" class="form-input" oninput="checkFormInput(event)">
-			<span id="email-error"></span>
+			<span class="form-text">Email</span><input required type="email" name="email" class="form-input" onblur="checkEmailInput(this)" onchange="checkEmailInput(this)">
+			<span class="form-error" id="email-error"></span>
 		</div>
 		<input type="submit" text="Register">
 	</form>
--- main.js
+++ main.js
@@ -1,25 +1,21 @@
-function checkFormInput(e){
-	var pattern = document.activeElement.getAttribute("data-allow");
-	var limit = document.activeElement.getAttribute("data-limit");
+function checkFormInput(el){
+	var pattern = el.getAttribute("data-allow");
+	var limit = el.getAttribute("data-limit");
 	var regex = new RegExp(pattern, "i");
-	var input = document.activeElement.value;
+	var input = el.value;
 
-	var errorPlace = document.getElementById(document.activeElement.getAttribute("name") + "-error");
-	console.log(input.length + " " + limit);
-
+	var errorPlace = document.getElementById(el.getAttribute("name") + "-error");
 
 	// Set first char to be uppercase
-	if(input.length === 1){
-		document.activeElement.value = input.toUpperCase();
-	}
+	el.value = input.charAt(0).toUpperCase() + input.slice(1);
 
 	var valid = regex.test(input);
 	
 	if(input.length === 0){
 		errorPlace.innerHTML = "Required, cannot be blank";
 	}
-	else if (input.length > limit){
-		document.activeElement.value = input.substring(0, input.length - 1);
+	else if (limit != 0 && input.length > limit){
+		el.value = input.substring(0, input.length - 1);
 		errorPlace.innerHTML = "Over limit. You can only type " + limit + " characters";
 	}
 	else if (valid){
@@ -28,4 +24,57 @@ function checkFormInput(e){
 	else{
 		errorPlace.innerHTML = "Wrong Input";
 	}
+}
+
+function checkPhoneInput(el){
+	var regex = new RegExp("^[0-9]+$", "i");
+	var input = el.value;
+
+	var errorPlace = document.getElementById(el.getAttribute("name") + "-error");
+
+	var valid = regex.test(input);
+	
+	if(input.length === 0){
+		errorPlace.innerHTML = "Required, cannot be blank";
+	}
+	else if (10 != 0 && input.length > 10){
+		el.value = input.substring(0, input.length - 1);
+		errorPlace.innerHTML = "Over limit. You can only type 10 characters";
+	}
+	else if (valid){
+		errorPlace.innerHTML = "";
+	}
+	else{
+		errorPlace.innerHTML = "Wrong Input";
+	}
+}
+
+function checkEmailInput(el){
+	// http://thedailywtf.com/Articles/Validating_Email_Addresses.aspx 
+	var regex = new RegExp("^[-!#$%&'*+/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z{|}~])*@[a-zA-Z](-?[a-zA-Z0-9])*(\.[a-zA-Z](-?[a-zA-Z0-9])*)+$", "i");
+	var input = el.value;
+	var valid = regex.test(input);
+	var errorPlace = document.getElementById(el.getAttribute("name") + "-error");
+
+	if (valid){
+		errorPlace.innerHTML = "";
+	}
+	else if(input.length === 0){
+		errorPlace.innerHTML = "Required, cannot be blank";
+	}
+	else{
+		errorPlace.innerHTML = "Wrong Input";
+	}
+}
+
+function formCheck(el){
+	var elements = el.querySelectorAll("span.form-error");
+	var bCheck = true;
+	for (var i; i < elements.length; i++){
+		if (elements[i].innerHTML !== ""){
+			bCheck = false;
+			break;
+		}
+	}
+	return bCheck;
 }
